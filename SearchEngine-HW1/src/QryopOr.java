5b2b1ff9bbafde7bf6eeacfb66730ed525a0746f
JerrySun363 <sunchen363@gmail.com>
2013-09-14 12:08:06 -0400

    all new

---
+++ SearchEngine-HW1/src/QryopOr.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+/**
+ * 
+ */
+
+/**
+ * @author jerry
+ *
+ */
+public class QryopOr extends Qryop {
+
+  /* (non-Javadoc)
+   * @see Qryop#evaluate()
+   */
+  @Override
+  public QryResult evaluate() throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
